db8ddf6a34d54eab5b943077fe6726295270d3a4
Baptiste 'Colonel' Mouterde <baptiste.mouterde@gmail.com>
2014-02-25 14:48:45 +0100

    init commit

---
+++ Model/XmlLoader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolo
+ * Date: 25/02/14
+ * Time: 14:48
+ */
+
+namespace Model;
+
+
+class XmlLoader {
+
+} 
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolo
+ * Date: 25/02/14
+ * Time: 14:45
+ */
+
+
+
+?>
