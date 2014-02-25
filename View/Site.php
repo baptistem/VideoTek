e704d70295b62c37e85074f808dddc4a910dea91
Baptiste 'Colonel' Mouterde <baptiste.mouterde@gmail.com>
2014-02-25 15:13:38 +0100

    add site view

---
+++ View/Site.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolo
+ * Date: 25/02/14
+ * Time: 15:10
+ */
+
+namespace View;
+
+
+class Site {
+
+    public $header;
+    public $content;
+
+    function __construct()
+    {
+
+    }
+} 
